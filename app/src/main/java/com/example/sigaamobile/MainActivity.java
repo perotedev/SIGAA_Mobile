1ff72c465b51b3bf68f65e7f0ac58603e209e7d3
perotedev <rodripema@hotmail.com>
2022-11-20 19:49:09 -0400

    Fazendo funcionar a navegação entre fragments

--- app/src/main/java/com/example/sigaamobile/MainActivity.java
+++ app/src/main/java/com/example/sigaamobile/MainActivity.java
@@ -13,16 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        if (savedInstanceState == null) {
-//            getSupportFragmentManager().beginTransaction()
-//                    .replace(R.id.container, MenuFragment.newInstance())
-//                    .commitNow();
-//        }
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, LoginFragment.newInstance())
-                    .commitNow();
-        }
     }
 }
--- app/src/main/java/com/example/sigaamobile/ui/main/LoginFragment.java
+++ app/src/main/java/com/example/sigaamobile/ui/main/LoginFragment.java
@@ -1,14 +1,13 @@
 package com.example.sigaamobile.ui.main;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
-
+import androidx.navigation.fragment.NavHostFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.sigaamobile.R;
 
 public class LoginFragment extends Fragment {
@@ -21,11 +20,16 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        AppCompatButton btnEntrar = view.findViewById(R.id.btn_entrar);
+
+        btnEntrar.setOnClickListener(v -> {
+            NavHostFragment.findNavController(LoginFragment.this).navigate(R.id.fragment_menu);
+        });
     }
 }
