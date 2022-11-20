99495f526065fcf17e1579bff21791e470fb174a
perotedev <rodripema@hotmail.com>
2022-11-20 08:42:30 -0400

    Criando esqueleto da aplicação

---
+++ app/src/main/java/com/example/sigaamobile/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sigaamobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sigaamobile.ui.main.MenuFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MenuFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sigaamobile/ui/main/MenuFragment.java
@@ -0,0 +1,39 @@
+package com.example.sigaamobile.ui.main;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sigaamobile.R;
+
+public class MenuFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+
+    public static MenuFragment newInstance() {
+        return new MenuFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+
+}
