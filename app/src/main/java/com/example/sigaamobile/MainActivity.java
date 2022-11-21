7a51525e85f79a4ac11a06218a34bc4ef33f72b4
perotedev <rodripema@hotmail.com>
2022-11-20 20:47:31 -0400

    Adicionando logo vazada do sigaa

--- app/src/main/java/com/example/sigaamobile/MainActivity.java
+++ app/src/main/java/com/example/sigaamobile/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.sigaamobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.sigaamobile.ui.main.LoginFragment;
 import com.example.sigaamobile.ui.main.MenuFragment;
@@ -13,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void voltar(View v){
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, LoginFragment.newInstance())
+                .commitNow();
+//        getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+//        int count = getFragmentManager().getBackStackEntryCount();
+//        if (count == 0)
+//            super.onBackPressed();
     }
 }
