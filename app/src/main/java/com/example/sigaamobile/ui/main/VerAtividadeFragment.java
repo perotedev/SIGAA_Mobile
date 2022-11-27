8f1c6769032f3ead6e5a85dab7c623ba51555202
perotedev <rodripema@hotmail.com>
2022-11-26 22:47:51 -0400

    Abrindo explorador de arquivos para selecionar o PDF

--- app/src/main/java/com/example/sigaamobile/ui/main/VerAtividadeFragment.java
+++ app/src/main/java/com/example/sigaamobile/ui/main/VerAtividadeFragment.java
@@ -1,5 +1,6 @@
 package com.example.sigaamobile.ui.main;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,7 +10,10 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
@@ -18,10 +22,42 @@ import androidx.fragment.app.Fragment;
 import com.example.sigaamobile.MainActivity;
 import com.example.sigaamobile.R;
 import com.example.sigaamobile.models.mAtividade;
+import com.example.sigaamobile.utils.AttatchDocument;
 import com.example.sigaamobile.utils.DateTransform;
 import com.google.gson.Gson;
 
 public class VerAtividadeFragment extends Fragment {
+    private final AttatchDocument document = new AttatchDocument();
+    ActivityResultLauncher<Intent> someActivityResultLauncher;
+    TextView textAreaDoc;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // this line is because in fragments registerForActivityResult() must to be called before view is created
+        this.someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    String docName = "documento_teste.pdf";
+                    System.out.println("resultado: "+data);
+
+                    if (data != null) {
+                        this.document.setDocumentDirectoy(data.getData().getPath());
+                        this.document.setDocumentSize();
+                        textAreaDoc.setText(docName);
+                        this.showToast("Documento "+docName+" anexado!");
+                    } else {
+                        this.showToast("Erro ao anexar documento!");
+                    }
+                } else {
+                    this.showToast("Erro ao anexar documento!");
+                }
+            }
+        );
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -49,9 +85,9 @@ public class VerAtividadeFragment extends Fragment {
         TextView fimAtividade = view.findViewById(R.id.data_fim_atividade);
         TextView statusAtividade = view.findViewById(R.id.status_da_atividade);
         ImageView btnArrowRight = view.findViewById(R.id.btn_ir_atividade);
-        TextView textAreaDoc = view.findViewById(R.id.doc_anexado);
         RelativeLayout btnSelectDoc = view.findViewById(R.id.btn_selecionar_doc);
         AppCompatButton btnAnexoAtividade = view.findViewById(R.id.btn_baixar_anexo_atividade);
+        this.textAreaDoc = view.findViewById(R.id.doc_anexado);
 
         btnArrowRight.setVisibility(View.INVISIBLE);
         titleDetalheCard.setHeight(0);
@@ -66,12 +102,11 @@ public class VerAtividadeFragment extends Fragment {
         descricaoAtividade.setLayoutParams(margins);
 
         textAreaDoc.setOnClickListener(v -> {
-//            String path = Environment.getExternalStorageDirectory()+"/teste_sigaa/";
-//            Uri uri = Uri.parse(path);
-            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.setType("*/*");
-            intent = Intent.createChooser(intent, "Choose a file");
-            startActivity(intent);
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("application/pdf");
+            intent = Intent.createChooser(intent, "Selecionar arquivo...");
+            this.someActivityResultLauncher.launch(intent);
         });
 
         btnSelectDoc.setOnClickListener(v -> {
@@ -86,6 +121,9 @@ public class VerAtividadeFragment extends Fragment {
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
             requireContext().startActivity(browserIntent);
         });
+    }
 
+    private void showToast(String message){
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/sigaamobile/utils/AttatchDocument.java
@@ -0,0 +1,49 @@
+package com.example.sigaamobile.utils;
+
+import android.net.Uri;
+
+import java.io.File;
+
+public class AttatchDocument {
+    private Uri documentDirectoy;
+    private String documentName;
+    private double documentSize;
+
+    // Setters
+    public void setDocumentDirectoy(String documentDirectoy){
+        setDocumentName(documentDirectoy);
+        this.documentDirectoy = Uri.parse(documentDirectoy);
+    }
+
+    public void setDocumentName(String documentName){
+        this.documentName = documentName;
+
+    }
+
+    public void setDocumentSize(){
+        File file = new File(getDocumentDirectoy().toString());
+        this.documentSize =  file.length();
+    }
+
+    //Getters
+    public Uri getDocumentDirectoy() {
+        return this.documentDirectoy;
+    }
+
+    public String getDocumentName(){
+        String[] documentNameList = this.documentName.split("/");
+        int sizeList = documentNameList.length;
+        return documentNameList[sizeList-1];
+    }
+
+    public double getDocumentSize(){
+        return this.documentSize;
+    }
+
+    public float getDocumentSizeMb(){
+        float documentSizeMb;
+        String sizeString = String.valueOf((getDocumentSize()/1024)/1024);
+        documentSizeMb = Float.parseFloat(sizeString);
+        return documentSizeMb;
+    }
+}
